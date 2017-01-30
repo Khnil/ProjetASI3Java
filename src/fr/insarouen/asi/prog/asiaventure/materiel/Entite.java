d1b29bd209fe93f0c466da14d6141f39065771ea
Tom Masson <Tom@MacBook-Pro-Tom.local>
2017-01-30 15:52:24 +0100

    aze

--- src/fr/insarouen/asi/prog/asiaventure/materiel/Entite.java
+++ src/fr/insarouen/asi/prog/asiaventure/materiel/Entite.java
@@ -3,9 +3,27 @@ package fr.insarouen.asi.prog.asiaventure.materiel;
 import java.lang.string;
 
 public class Entite{
-    private string nom;
+    private String nom;
+    private Monde monde;
 
     public Entite(String nom, Monde monde){
+        this.nom=nom;
+        this.monde=monde;
+    }
 
+    public String getMonde(){
+        return monde;
+    }
+
+    public String getNom(){
+        return nom;
+    }
+
+    public String toString(){
+
+    }
+
+    public Boolean equals(){
+        
     }
 }
