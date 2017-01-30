981aa193fcb90559e30d38351f42d7074b028769
Tom Masson <Tom@MacBook-Pro-Tom.local>
2017-01-30 18:09:47 +0100

    pieddebiche

--- src/fr/insarouen/asi/prog/asiaventure/materiel/PiedDeBiche.java
+++ src/fr/insarouen/asi/prog/asiaventure/materiel/PiedDeBiche.java
@@ -2,3 +2,12 @@ package fr.insarouen.asi.prog.asiaventure.materiel.objets;
 
 import fr.insarouen.asi.prog.asiaventure.materiel.Entite;
 import fr.insarouen.asi.prog.asiaventure.materiel.Monde;
+
+public class PiedDeBiche extends Objet {
+    public PiedDeBiche(String nom, Monde monde) throws NomDEntiteDejaUtiliseDansLeMondeException{
+      super(nom,monde);
+    }
+    public boolean estDeplacable(){
+      return true;
+    }
+  }
