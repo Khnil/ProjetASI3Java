3a506f1716e2ecb554e43eb6c811db01127f7ced
Tom Masson <Tom@MacBook-Pro-Tom.local>
2017-02-01 14:02:01 +0100

    doc

--- src/fr/insarouen/asi/prog/asiaventure/materiel/objets/Objet.java
+++ src/fr/insarouen/asi/prog/asiaventure/materiel/objets/Objet.java
@@ -3,9 +3,33 @@ package fr.insarouen.asi.prog.asiaventure.materiel.objets;
 import fr.insarouen.asi.prog.asiaventure.materiel.Entite;
 import fr.insarouen.asi.prog.asiaventure.Monde;
 
+/**
+ * Classe de base pour représenter les objets.<br>
+ * Cette classe hérite de la classe Entite (avec la déplacabilité en plus). <br>
+ *
+ * @see Entite
+ *
+ * @author Tom / Constantin
+ * @version 1.0
+ */
 public abstract class Objet extends Entite {
+
+    /**
+     * Constructeur Objet. <br>
+     * A la construction d'un objet Objet, le constructeur de la classe Entite est appelé
+     * avec le nom de l'Objet et le monde concerné.
+     *
+     * @param nom
+     *        Nom de l'Objet
+     * @param monde
+     *        Monde concerné
+     */
   public Objet(String nom, Monde monde){
     super(nom,monde);
   }
+
+  /**
+   * Retourne un booléen suivant la déplacabilité de l'Objet.
+   */
   public abstract boolean estDeplacable();
 }
