930015bc395734247e72e02b4353e832fff9ea05
Tom Masson <Tom@MacBook-Pro-Tom.local>
2017-02-01 14:01:48 +0100

    suite doc

--- src/fr/insarouen/asi/prog/asiaventure/materiel/structure/ElementStructurel.java
+++ src/fr/insarouen/asi/prog/asiaventure/materiel/structure/ElementStructurel.java
@@ -3,7 +3,24 @@ package fr.insarouen.asi.prog.asiaventure.materiel.structure;
 import fr.insarouen.asi.prog.asiaventure.materiel.Entite;
 import fr.insarouen.asi.prog.asiaventure.Monde;
 
-public abstract class ElementStructurel extends Entite {
+/**
+ * Classe qui permet de décrire la structure d'un élement. <br>
+ *
+ * @see Entite
+ *
+ * @author Tom / Constantin
+ * @version 1.0
+ */
+public class ElementStructurel extends Entite {
+
+    /**
+     * Lors de la construction d'un objet ElementStructurel, le constructeur de la classe Entite est appelé avec le nom de l'elementStructurel et le monde associé.
+     *
+     * @param nom
+     *        Le nom de l'ElementStructurel
+     * @param monde
+     *        Le monde concerné
+     */
   public ElementStructurel (String nom, Monde monde){
     super(nom,monde);
   }
