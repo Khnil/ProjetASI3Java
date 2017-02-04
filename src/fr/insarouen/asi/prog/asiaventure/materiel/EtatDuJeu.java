fe7cbb92529de0da06be7457a137b49dd69457ef
cdedeyan <cdedeyan@insa-rouen.fr>
2017-02-04 14:32:59 +0100

    tentative de prise en main de github

--- src/fr/insarouen/asi/prog/asiaventure/materiel/EtatDuJeu.java
+++ src/fr/insarouen/asi/prog/asiaventure/materiel/EtatDuJeu.java
@@ -1,3 +1,10 @@
 package fr.insarouen.asi.prog.asiaventure.materiel;
 
+/**
+  * Classe definissant si une partie est reussie, perdue ou pas encore finie
+  *
+  * @author Tom/Constantin
+  *
+  * @version 1.0
+  */
 public enum EtatDuJeu {ECHEC, ENCOURS, SUCCES};
