9a8cb9fe6188353c970f536f3f5d968249a4d6dd
cdedeyan <constantindedeyan@gmail.com>
2017-02-06 18:00:54 +0100

    ajout des tests Vivant(non finis)

---
+++ src/fr/insarouen/asi/prog/asiaventure/materiel/vivants/ClassMainTestVivant.java
@@ -0,0 +1,20 @@
+package fr.insarouen.asi.prog.asiaventure.materiel.objets;
+
+import fr.insarouen.asi.prog.asiaventure.materiel.objets.Vivant;
+import fr.insarouen.asi.prog.asiaventure.materiel.Entite;
+import fr.insarouen.asi.prog.asiaventure.Monde;
+
+public class ClassMainTestVivant{
+    public static void main ( String[] args ){
+        Monde maison = new Monde("Maison");
+        Piece chambre = new Piece("Chambre",maison);
+        PiedDeBiche pourCasserLaPorte = new PiedDeBiche("PiedDeBiche",maison);
+        PiedDeBiche pourTaperMonFrere = new PiedDeBiche("Ouch",maison);
+        Vivant moi = new Vivant("ConstanTom",maison,20,5,chambre,pourTaperMonFrere);
+        Vivant frere = new Vivant("Frangin",maison,20,5,chambre);
+        chambre.deposer(pourCasserLaPorte);
+
+    }
+}
+
+//pas fini
