682b5c313f69323d8301168e607772b67a83e9c2
cdedeyan <constantindedeyan@gmail.com>
2017-03-06 16:17:18 +0100

    ajout de TestMonde

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ doc: javadoc -encoding utf8 -docencoding utf8 -charset utf8 -d ./doc -sourcepath
 Mac Tom TEST COMPILATION: javac -classpath /Users/Tom/Java/jUnit/hamcrest.jar:/Users/Tom/Java/jUnit/junit.jar:./classes:./classestest -sourcepath ./testsrc -d classestest $(find testsrc -name *.java)
 
 Mac Tom TEST EXECUTION: java -classpath /Users/Tom/Java/jUnit/hamcrest.jar:/Users/Tom/Java/jUnit/junit.jar:./classes:./classestest  org.junit.runner.JUnitCore fr.insarouen.asi.prog.asiaventure.materiel.TestEntite
+
+Linux Const TEST EXECUTION: java -classpath ./testClasses:./classes:/usr/share/java/junit4.jar:/usr/share/java/hamcrest-library.jar org.junit.runner.JUnitCore fr.insarouen.asi.prog.asiaventure.TestMonde 
---
+++ testSrc/fr/insarouen/asi/prog/asiaventure/TestMonde.java
@@ -0,0 +1,32 @@
+package fr.insarouen.asi.prog.asiaventure;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.hamcrest.core.IsEqual;
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+import fr.insarouen.asi.prog.asiaventure.materiel.Entite;
+import fr.insarouen.asi.prog.asiaventure.NomDEntiteDejaUtiliseDansLeMondeException;
+
+public class TestMonde {
+    public Monde monde;
+
+    @Before
+    public void avantTest() throws
+    Exception{
+        monde = new Monde("Monde");
+    }
+
+    @Test
+    public void testConstructeur(){
+        assertThat(monde.getNom(),IsEqual.equalTo("Monde"));
+    }
+
+    @Test
+    public void testAjouter() throws NomDEntiteDejaUtiliseDansLeMondeException{
+        Entite entite = new Entite("truc",monde){};
+        assertThat(monde.getEntite("truc"),IsEqual.equalTo(entite));
+    }
+}
