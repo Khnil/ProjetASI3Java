cb070c966829c6ef69cc0707b574c0dae5727439
cdedeyan <constantindedeyan@gmail.com>
2017-03-06 17:45:49 +0100

    Maj de TestPiece

--- testSrc/fr/insarouen/asi/prog/asiaventure/materiel/structure/TestPiece.java
+++ testSrc/fr/insarouen/asi/prog/asiaventure/materiel/structure/TestPiece.java
@@ -10,6 +10,10 @@ import static org.hamcrest.core.Is.is;
 import fr.insarouen.asi.prog.asiaventure.materiel.structure.Piece;
 import fr.insarouen.asi.prog.asiaventure.Monde;
 import fr.insarouen.asi.prog.asiaventure.materiel.objets.PiedDeBiche;
+import fr.insarouen.asi.prog.asiaventure.NomDEntiteDejaUtiliseDansLeMondeException;
+import fr.insarouen.asi.prog.asiaventure.EntiteDejaDansUnAutreMondeException;
+import fr.insarouen.asi.prog.asiaventure.materiel.structure.ObjetAbsentDeLaPieceException;
+import fr.insarouen.asi.prog.asiaventure.materiel.objets.ObjetNonDeplacableException;
 
 public class TestPiece {
     private Piece piece;
@@ -23,23 +27,31 @@ public class TestPiece {
     }
 
     @Test
-    public void testConstructeur() throws Exception{
+    public void testConstructeur() throws NomDEntiteDejaUtiliseDansLeMondeException,EntiteDejaDansUnAutreMondeException{
         assertThat(piece.getNom(),IsEqual.equalTo("classe"));
         assertThat(piece.getMonde(),IsEqual.equalTo(monde));
     }
 
     @Test
-    public void testDeposer() throws Exception{
+    public void testDeposer() throws NomDEntiteDejaUtiliseDansLeMondeException,EntiteDejaDansUnAutreMondeException{
         PiedDeBiche objet = new PiedDeBiche("pied",monde);
         piece.deposer(objet);
         assertThat(piece.contientObjet("pied"),IsEqual.equalTo(true));
     }
 
     @Test
-    public void testRetirer() throws Exception{
+    public void testRetirer() throws EntiteDejaDansUnAutreMondeException,NomDEntiteDejaUtiliseDansLeMondeException,ObjetNonDeplacableException,ObjetAbsentDeLaPieceException{
         PiedDeBiche objet = new PiedDeBiche("pied",monde);
         piece.deposer(objet);
         piece.retirer(objet);
         assertThat(piece.contientObjet("pied"),IsEqual.equalTo(false));
+    }
+
+    @Test(expected =ObjetAbsentDeLaPieceException.class)
+    public void testRetirerObjetAbsent() throws ObjetAbsentDeLaPieceException,ObjetNonDeplacableException,NomDEntiteDejaUtiliseDansLeMondeException,EntiteDejaDansUnAutreMondeException {
+        PiedDeBiche objet1 = new PiedDeBiche("pied",monde);
+        PiedDeBiche objet2 = new PiedDeBiche("pied",monde);
+        piece.deposer(objet2);
+        piece.retirer(objet1);
     }
 }
